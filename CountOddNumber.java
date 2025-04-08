b6865b7b16bb028c73f48cca453c7458b42a37ab
priyanankushkakamble <priyankakamble18042003@gmail.com>
2025-04-08 23:39:48 +0530

    CountOddNumber.java

---
+++ CountOddNumber.java
@@ -0,0 +1,30 @@
+package ArraysProgram;
+import java.util.*;
+public class CountOddNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner xyz=new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int size=xyz.nextInt();
+		int a[]=new int[size];
+		int count=0;
+		System.out.println("Enter the array element");
+		for(int i=0;i<size;i++)
+		{
+			a[i]=xyz.nextInt();
+		}
+		System.out.println("Display array element");
+		for(int i=0;i<size;i++)
+		{
+			System.out.printf("%d\t",a[i]);
+		}
+		System.out.println("\n");
+		for(int i=0;i<size;i++)
+		if(a[i]%2!=0)
+		{
+			count++;
+		}
+		System.out.println("Count of odd number is:"+count);	
+	}
+}
